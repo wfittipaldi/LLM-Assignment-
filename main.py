24e5be3d343711808882f752bab00f0c64da7d22
Curtis Barnhart <cbarnhart@westmont.edu>
2024-12-05 14:31:57 -0800

    I added stubs for each of us to work on.

---
+++ main.py
@@ -0,0 +1,20 @@
+def process_input(fname: str) -> list[str]:
+    """
+    Levi
+    """
+    pass
+
+
+def ollama_batch(items: list[str], n: int) -> list[str]:
+    """
+    Curtis
+    """
+    pass
+
+
+def ollama_reduce(items: list[str], n: int) -> list[str]:
+    """
+    Will
+    """
+    pass
+
