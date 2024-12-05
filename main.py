4f8dbcfe5533ea52e1e639f90b11abdb74aa546f
Levi Wicks <lwicks@westmont.edu>
2024-12-05 14:50:44 -0800

    completed the process input function

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ def process_input(fname: str) -> list[str]:
     """
     Levi
     """
-    pass
-
+    with open(fname, 'r', encoding='utf-8') as f:
+        file_text = f.read() 
+        return file_text.split('\n---\n')
 
 def ollama_batch(items: list[str], n: int) -> list[str]:
     """
